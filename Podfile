8e5f342f3fa111e6ad04715d63c3700393f15c57
terryxu <terry.dingli.xu@accenture.com>
2021-07-20 02:21:55 +0800

    RedMachine-1: Initialize project.

---
+++ Podfile
@@ -0,0 +1,9 @@
+project 'RedMachine/RedMachine.xcodeproj'
+platform :ios, '11.0'
+
+use_frameworks!
+target 'RedMachine' do
+pod 'RxSwift', '= 6.1.0'
+pod 'RxCocoa', '= 6.1.0'
+end
+	
---
+++ RedMachine/RedMachine/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RedMachine
+//
+//  Created by Terry Xu on 2021/7/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+
+}
+
